0fa90bed0ae50d18b4dc2092db5bee4f3137f634
Samuel Pascua <pascua.samuel.14@gmail.com>
2023-10-06 11:33:47 +0800

    Revert "BACKPORT: sched/fair: Fix overutilized update in enqueue_task_fair()"
    This reverts commit 444683e9df5852431539e1814df912759cf80370.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5579,7 +5579,6 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	struct sched_entity *se = &p->se;
 	bool prefer_idle = sched_feat(EAS_PREFER_IDLE) ?
 				(schedtune_prefer_idle(p) > 0) : 0;
-	int task_new = !(flags & ENQUEUE_WAKEUP);
 
 	/*
 	 * The code below (indirectly) updates schedutil which looks at
@@ -5672,8 +5671,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		 * overutilized. Hopefully the cpu util will be back to
 		 * normal before next overutilized check.
 		 */
-		if (!task_new &&
-		    !(prefer_idle && rq->nr_running == 1))
+		if ((flags & ENQUEUE_WAKEUP) && !(prefer_idle && rq->nr_running == 1))
 			update_overutilized_status(rq);
 	}
 
